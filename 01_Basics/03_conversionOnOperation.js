86748bedbc9c5c8f4c057fdd129f103b7191d0c4
AYUSH ANAND <156613783+Ayush45Anand@users.noreply.github.com>
2025-08-27 19:34:05 +0000

    commit

---
+++ 01_Basics/03_conversionOnOperation.js
@@ -0,0 +1,58 @@
+let score= "ayush"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"45" => 45
+// "45ayu" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = ""
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1=> true; 0 => flase
+// "" => false
+// "ayush" => true
+
+let someNumber = 45
+let stringNumber =String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+//**********************Operations******************8
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+//console.log((2+2));
+//console.log((2**4)); //  2 to the power 4
+// console.log((2%3));
+
+let str1 = "hello"
+let str2 = " ayush"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log(1 + 2 + "3"); 
+
+console.log((3 + 2) * 5 % 3);
+console.log(+true);
+
+
+
+
+
+
+
+
+
+
